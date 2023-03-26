155b285b2870d3d47c701a231b9fc9d922465894
vversu <polinakosuha@gmail.com>
2023-03-26 19:42:55 +0500

    скопировала проект из hw1

---
+++ project/link-parser/src/main/java/parsers/LinkParser.java
@@ -0,0 +1,22 @@
+package parsers;
+
+import java.net.MalformedURLException;
+import java.util.List;
+
+public class LinkParser {
+
+    List<LinkHandler> handlers;
+
+    public LinkParser() {
+        handlers = List.of(new GitHubLinkHandler(), new StackOverflowLinkHandler());
+    }
+
+    public Object parse(String url) throws MalformedURLException {
+    	  for (LinkHandler handler : handlers) {
+    	    if (handler.canHandle(url)) {
+    	      return handler.handle(url);
+    	    }
+    	  }
+    	  return null;
+    }
+}
