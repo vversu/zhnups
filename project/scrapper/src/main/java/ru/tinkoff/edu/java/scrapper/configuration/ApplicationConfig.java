40349df579dc73f6bdddc2ac5b6a5d218bcdec03
vversu <polinakosuha@gmail.com>
2023-03-28 23:40:28 +0500

    Revert "проверка"
    This reverts commit 987f2c5dd7e8040a8cf8a4f6f6fb5bc3dfcaede1.

--- project/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/configuration/ApplicationConfig.java
+++ project/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/configuration/ApplicationConfig.java
@@ -7,4 +7,4 @@ import org.springframework.validation.annotation.Validated;
 
 @Validated
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
-public record  ApplicationConfig(@NotNull String test, @NotBlank String schedulerInterval) {}
+public record ApplicationConfig(@NotNull String test, @NotBlank String schedulerInterval) {}
