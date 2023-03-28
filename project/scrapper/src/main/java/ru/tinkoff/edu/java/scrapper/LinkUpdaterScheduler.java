8022973adacd4fd34774c5e18e28c6067adbf6c9
vversu <polinakosuha@gmail.com>
2023-03-28 23:18:37 +0500

    часть 4 задания

---
+++ project/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/LinkUpdaterScheduler.java
@@ -0,0 +1,18 @@
+package ru.tinkoff.edu.java.scrapper;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LinkUpdaterScheduler {
+
+    private final Logger logger = LoggerFactory.getLogger(LinkUpdaterScheduler.class);
+
+    @Scheduled(fixedDelayString = "${app.scheduler.interval}")
+    public void update() {
+        logger.info("Updating links...");
+        // your code here
+    }
+}
