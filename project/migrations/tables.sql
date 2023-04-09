60b33b7d02094a11153965745e1e9ddd5d9a35b1
vversu <polinakosuha@gmail.com>
2023-04-09 23:06:02 +0500

    задание 3

--- project/migrations/tables.sql
+++ project/migrations/tables.sql
@@ -1,22 +1,18 @@
---liquibase formatted sql
---changeset your.name:create-tables
 CREATE TABLE link (
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-user_id INTEGER NOT NULL,
-url TEXT NOT NULL
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  url TEXT NOT NULL
 );
 
---changeset your.name:create-chat-table
 CREATE TABLE chat (
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-user_id INTEGER NOT NULL
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL
 );
 
---changeset your.name:create-link-tracking-table
 CREATE TABLE link_tracking (
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-link_id INTEGER NOT NULL,
-chat_id INTEGER NOT NULL,
-FOREIGN KEY (link_id) REFERENCES link(id),
-FOREIGN KEY (chat_id) REFERENCES chat(id)
+  id SERIAL PRIMARY KEY,
+  link_id INTEGER NOT NULL,
+  chat_id INTEGER NOT NULL,
+  FOREIGN KEY (link_id) REFERENCES link(id),
+  FOREIGN KEY (chat_id) REFERENCES chat(id)
 );
